4a37ce8ba591b4fa3774825bd17ea78a185685dc
Tanat Tonguthaisri <ii@ieee.org>
2023-03-27 11:51:30 +0700

    Experimental stage
    Update for experimental homework.

--- G.py
+++ G.py
@@ -9,9 +9,10 @@ spark = SparkSession(sc)
 import os
 
 
-os.system('wget https://storage.googleapis.com/2022oct23/LoanStats_web.csv')
+#os.system('wget https://storage.googleapis.com/2022oct23/LoanStats_web.csv')
+os.system('wget https://storage.cloud.google.com/2022oct23/Sony_ChurnPrediction.csv')
 os.system('hdfs dfs -mkdir -p /rawzone/')
-os.system('hdfs dfs -put LoanStats_web.csv /rawzone/')
+os.system('hdfs dfs -put Sony_ChurnPrediction.csv /rawzone/')
 
 
 from pyspark.sql import functions as F
@@ -20,199 +21,98 @@ from pyspark.sql import functions as F
 #                                       inferSchema=True, mode='DROPMALFORMED')
 
 
-raw_LendingClubWeb_df = spark.read.format('csv').\
+df = spark.read.format('csv').\
 option('header','true').option('mode','DROPMALFORMED')\
-.load('/rawzone/LoanStats_web.csv')
-
-
-#raw_LendingClubWeb_df.count()
-
-loanPayment_df = raw_LendingClubWeb_df\
-.filter((F.col('loan_status') == 'Fully Paid') | ((F.col('loan_status') =='Charged Off')))
-
-business_df = loanPayment_df.select('annual_inc'\
-                                           ,'bc_util'\
-                                           ,'inq_fi'\
-                                           ,'inq_last_12m'\
-                                           ,'home_ownership'\
-                                           ,'purpose'\
-                                           ,'emp_length'\
-                                           ,'revol_bal'\
-                                           ,'dti'\
-                                           ,'delinq_2yrs'\
-                                           ,'pub_rec_bankruptcies'\
-                                           ,'pub_rec'\
-                                           ,'open_rv_24m'\
-                                           ,'mort_acc'\
-                                           ,'num_actv_bc_tl'\
-                                           ,'num_actv_rev_tl'\
-                                           ,'num_il_tl'\
-                                           ,'num_tl_90g_dpd_24m'\
-                                           ,'int_rate'\
-                                           ,'inq_last_6mths'\
-                                           ,'term'\
-                                           ,'installment'\
-                                           ,'total_rev_hi_lim'\
-                                           ,'total_bal_il'\
-                                           ,'total_bal_ex_mort'\
-                                           ,'total_acc'\
-                                           ,'tot_cur_bal'\
-                                           ,'loan_amnt'\
-                                           ,'loan_status'\
-                                           ,'verification_status'\
-                                           ,'collections_12_mths_ex_med'\
-                                           ,'chargeoff_within_12_mths'\
-                                           ,'il_util'\
-                                           ,'last_pymnt_amnt'\
-                                           #,'last_pymnt_d'\
-                                           ,'out_prncp_inv'\
-                                           ,'out_prncp'\
-                                           ,'total_pymnt_inv'\
-                                           ,'total_pymnt'\
-                                           ,'grade')
-
+.load('/rawzone/Sony_ChurnPrediction.csv')
 
 import pandas as pd
+import numpy as np
+# set random seed to have reproducible results
+# sklearn uses numpy random seed
+np.random.seed(42)
 
-no_null_df = business_df.dropna(how='any')
+df['area code'] = pd.to_numeric(df['area code'])
 
-from pyspark.sql.functions import *
+df_spark = spark.createDataFrame(df)
 
-no_wedding_df = no_null_df.filter(F.col('purpose') != 'wedding')
+from pyspark.ml.feature import OneHotEncoder, StringIndexer
+from pyspark.sql.functions import when, col
 
-fitmem_no_null_df = no_wedding_df.repartition(60)
+# Create a new DataFrame with OneHotEncoded 'area code' column
+encoder = OneHotEncoder(inputCols=["area code"], outputCols=["area_code_encoded"])
+df_encoded = encoder.fit(df_spark).transform(df_spark)
 
-cached_no_null_df = fitmem_no_null_df.cache()
+# Rename the encoded columns with a prefix
+#df_encoded = df_encoded.select([col(col_name).alias("area_code_" + col_name) if col_name == "area_code_encoded" else col_name for col_name in df_encoded.columns])
 
-from pyspark.sql.functions import udf
-from pyspark.sql.types import *
+# Convert 'voice mail plan' and 'international plan' columns to integer type
+df_encoded = df_encoded.withColumn("voice mail plan", when(df_encoded["voice mail plan"] == "no", 0).otherwise(1))
+df_encoded = df_encoded.withColumn("international plan", when(df_encoded["international plan"] == "no", 0).otherwise(1))
+df_encoded = df_encoded.withColumn("voice mail plan", df_encoded["voice mail plan"].cast("integer"))
+df_encoded = df_encoded.withColumn("international plan", df_encoded["international plan"].cast("integer"))
 
-def f_removepercent(origin):
-    return origin.rstrip('%')
+# Drop unnecessary columns
+df_final = df_encoded.drop("phone number", "state", "area code")
 
-removepercent = udf(lambda x: f_removepercent(x),StringType())
+from pyspark.sql.functions import when
+from pyspark.ml.feature import OneHotEncoder, StringIndexer, VectorAssembler, StandardScaler
+from pyspark.ml.classification import RandomForestClassifier, GBTClassifier
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
-def f_exrtractmonth(origin):
-    return origin.split('-')[0]
-
-exrtractmonth = udf(lambda x: f_exrtractmonth(x),StringType())
-
-def python_treatNA(origin):
-    if origin == 'n/a':
-        new = 'NotEmployed'
-    else:
-        new = origin
-    return new
-
-treatNA = udf(lambda x: python_treatNA(x),StringType())
-
-from pyspark.sql.functions import col
-
-crunched_df = cached_no_null_df.\
-withColumn('emp_length',treatNA(cached_no_null_df['emp_length'])).\
-withColumn('int_rate',removepercent(cached_no_null_df['int_rate']).cast(DoubleType())).\
-withColumn('dti',cached_no_null_df['dti'].cast(DoubleType())).\
-withColumn('revol_bal',cached_no_null_df['revol_bal'].cast(DoubleType())).\
-withColumn('pub_rec',cached_no_null_df['pub_rec'].cast(DoubleType())).\
-withColumn('total_bal_il',cached_no_null_df['total_bal_il'].cast(DoubleType())).\
-withColumn('tot_cur_bal',cached_no_null_df['tot_cur_bal'].cast(DoubleType())).\
-withColumn('total_acc',cached_no_null_df['total_acc'].cast(DoubleType())).\
-withColumn('total_bal_ex_mort',cached_no_null_df['total_bal_ex_mort'].cast(DoubleType())).\
-withColumn('total_rev_hi_lim',cached_no_null_df['total_rev_hi_lim'].cast(DoubleType())).\
-withColumn('num_actv_rev_tl',cached_no_null_df['num_actv_rev_tl'].cast(DoubleType())).\
-withColumn('num_actv_bc_tl',cached_no_null_df['num_actv_bc_tl'].cast(DoubleType())).\
-withColumn('num_il_tl',cached_no_null_df['num_il_tl'].cast(DoubleType())).\
-withColumn('pub_rec_bankruptcies',cached_no_null_df['pub_rec_bankruptcies'].cast(DoubleType())).\
-withColumn('delinq_2yrs',cached_no_null_df['delinq_2yrs'].cast(DoubleType())).\
-withColumn('open_rv_24m',cached_no_null_df['open_rv_24m'].cast(DoubleType())).\
-withColumn('num_tl_90g_dpd_24m',cached_no_null_df['num_tl_90g_dpd_24m'].cast(DoubleType())).\
-withColumn('inq_last_6mths',cached_no_null_df['inq_last_6mths'].cast(DoubleType())).\
-withColumn('bc_util',cached_no_null_df['bc_util'].cast(DoubleType())).\
-withColumn('mort_acc',cached_no_null_df['mort_acc'].cast(DoubleType())).\
-withColumn('inq_fi',cached_no_null_df['inq_fi'].cast(DoubleType())).\
-withColumn('last_pymnt_amnt',cached_no_null_df['last_pymnt_amnt'].cast(DoubleType())).\
-withColumn('out_prncp_inv',cached_no_null_df['out_prncp_inv'].cast(DoubleType())).\
-withColumn('out_prncp',cached_no_null_df['out_prncp'].cast(DoubleType())).\
-withColumn('total_pymnt_inv',cached_no_null_df['total_pymnt_inv'].cast(DoubleType())).\
-withColumn('total_pymnt',cached_no_null_df['total_pymnt'].cast(DoubleType())).\
-withColumn('il_util',cached_no_null_df['il_util'].cast(DoubleType())).\
-withColumn('chargeoff_within_12_mths',cached_no_null_df['chargeoff_within_12_mths'].cast(DoubleType())).\
-withColumn('collections_12_mths_ex_med',cached_no_null_df['collections_12_mths_ex_med'].cast(DoubleType())).\
-withColumn('loan_amnt',cached_no_null_df['loan_amnt'].cast(DoubleType())).\
-withColumn('inq_last_12m',cached_no_null_df['inq_last_12m'].cast(DoubleType())).\
-withColumn('installment',cached_no_null_df['installment'].cast(DoubleType())).\
-withColumn('annual_inc',cached_no_null_df['annual_inc'].cast(DoubleType()))
-
-from pyspark.sql.functions import *
-max_annual_inc = crunched_df.select(max('annual_inc')).collect()[0][0]
-min_annual_inc = crunched_df.select(min('annual_inc')).collect()[0][0]
-
-def t_annual_inc(origin):
-    return ((origin-min_annual_inc)/(max_annual_inc-min_annual_inc))
-
-n_annual_inc = udf(lambda x: t_annual_inc(x),DoubleType())
-
-max_revol_bal = crunched_df.select(max('revol_bal')).collect()[0][0]
-min_revol_bal = crunched_df.select(min('revol_bal')).collect()[0][0]
-
-def t_revol_bal(origin):
-    return ((origin-min_revol_bal)/(max_revol_bal-min_revol_bal))
-
-n_revol_bal = udf(lambda x: t_revol_bal(x),DoubleType())
-
-max_tot_cur_bal = crunched_df.select(max('tot_cur_bal')).collect()[0][0]
-min_tot_cur_bal = crunched_df.select(min('tot_cur_bal')).collect()[0][0]
-
-def t_tot_cur_bal(origin):
-    return ((origin-min_tot_cur_bal)/(max_tot_cur_bal-min_tot_cur_bal))
-
-n_tot_cur_bal = udf(lambda x: t_tot_cur_bal(x),DoubleType())
-
-max_total_rev_hi_lim = crunched_df.select(max('total_rev_hi_lim')).collect()[0][0]
-min_total_rev_hi_lim = crunched_df.select(min('total_rev_hi_lim')).collect()[0][0]
-
-def t_total_rev_hi_lim(origin):
-    return ((origin-min_total_rev_hi_lim)/(max_total_rev_hi_lim-min_total_rev_hi_lim))
-
-n_total_rev_hi_lim = udf(lambda x: t_total_rev_hi_lim(x),DoubleType())
-
-max_total_bal_ex_mort = crunched_df.select(max('total_bal_ex_mort')).collect()[0][0]
-min_total_bal_ex_mort = crunched_df.select(min('total_bal_ex_mort')).collect()[0][0]
-
-def t_total_bal_ex_mort(origin):
-    return ((origin-min_total_bal_ex_mort)/(max_total_bal_ex_mort-min_total_bal_ex_mort))
-
-n_total_bal_ex_mort = udf(lambda x: t_total_bal_ex_mort(x),DoubleType())
-
-max_total_bal_il = crunched_df.select(max('total_bal_il')).collect()[0][0]
-min_total_bal_il = crunched_df.select(min('total_bal_il')).collect()[0][0]
-
-def t_total_bal_il(origin):
-    return ((origin-min_total_bal_il)/(max_total_bal_il-min_total_bal_il))
-
-n_total_bal_il = udf(lambda x: t_total_bal_il(x),DoubleType())
-
-normalized_df = crunched_df.\
-withColumn('annual_inc',n_annual_inc(crunched_df['annual_inc'])).\
-withColumn('revol_bal',n_revol_bal(crunched_df['revol_bal'])).\
-withColumn('tot_cur_bal',n_tot_cur_bal(crunched_df['tot_cur_bal'])).\
-withColumn('total_rev_hi_lim',n_total_rev_hi_lim(crunched_df['total_rev_hi_lim'])).\
-withColumn('total_bal_il',n_total_bal_il(crunched_df['total_bal_il'])).\
-withColumn('total_bal_ex_mort',n_total_bal_ex_mort(crunched_df['total_bal_ex_mort']))
-
-normalized_filtered_df = normalized_df
-
-data_no_missing_df = normalized_filtered_df.dropna(how='any')
-
-# Update to your GCS bucket
-gcs_bucket = 'dataproc-bucket-name'
-
-gcs_filepath = 'gs://studentoct22-2/refinedzone/pim30_purpose.csv'.format(gcs_bucket)
-
-#data_no_missing_df.coalesce(1).write \
-#  .mode('overwrite') \
-#  .csv(gcs_filepath)
+# Create Spark DataFrame from pandas DataFrame df
+df_spark = spark.createDataFrame(df)
+df_spark = df_spark.withColumn("churn", df_spark["churn"].cast("integer"))
 
 
-normalized_df.write.mode('overwrite').parquet('/rawzone/')
+# One-hot encode area code
+area_indexer = StringIndexer(inputCol='area code', outputCol='area_code_index')
+area_encoder = OneHotEncoder(inputCol='area_code_index', outputCol='area_code_vec')
+area_encoder.setDropLast(False)
+
+# Convert categorical columns into numerical columns
+vm_indexer = StringIndexer(inputCol='voice mail plan', outputCol='voice_mail_plan_index')
+intl_indexer = StringIndexer(inputCol='international plan', outputCol='international_plan_index')
+label_indexer = StringIndexer(inputCol='churn', outputCol='label')
+
+# Assemble features vector
+assembler = VectorAssembler(inputCols=['account length', 'number vmail messages', 'total day minutes', 'total day calls',
+                                       'total day charge', 'total eve minutes', 'total eve calls', 'total eve charge',
+                                       'total night minutes', 'total night calls', 'total night charge', 'total intl minutes',
+                                       'total intl calls', 'total intl charge', 'customer service calls', 'area_code_vec',
+                                       'voice_mail_plan_index', 'international_plan_index'],
+                            outputCol='features')
+
+# Split data into training and test sets
+(training_data, test_data) = df_spark.randomSplit([0.8, 0.2], seed=42)
+
+# Standardize features
+scaler = StandardScaler(inputCol='features', outputCol='scaled_features')
+
+# Train random forest model
+rf = RandomForestClassifier(labelCol='label', featuresCol='scaled_features', maxDepth=5, seed=42)
+rf_pipeline = Pipeline(stages=[area_indexer, area_encoder, vm_indexer, intl_indexer, label_indexer, assembler, scaler, rf])
+rf_model = rf_pipeline.fit(training_data)
+'''
+rf_predictions = rf_model.transform(test_data)
+rf_evaluator = MulticlassClassificationEvaluator(labelCol='label', predictionCol='prediction', metricName='f1')
+rf_f1_score = rf_evaluator.evaluate(rf_predictions)
+'''
+
+rf_model.write().overwrite().save('gs://20221027-pim30-tanat-tonguthaisri-au-southeast1/refinedzone/Group3_rfModel')
+
+# Train gradient boosted tree model
+gbt = GBTClassifier(labelCol='label', featuresCol='scaled_features', maxDepth=5, seed=42)
+gbt_pipeline = Pipeline(stages=[area_indexer, area_encoder, vm_indexer, intl_indexer, label_indexer, assembler, scaler, gbt])
+gbt_model = gbt_pipeline.fit(training_data)
+'''
+gbt_predictions = gbt_model.transform(test_data)
+gbt_evaluator = MulticlassClassificationEvaluator(labelCol='label', predictionCol='prediction', metricName='f1')
+gbt_f1_score = gbt_evaluator.evaluate(gbt_predictions)
+'''
+
+gbt_model.write().overwrite().save('gs://20221027-pim30-tanat-tonguthaisri-au-southeast1/refinedzone/Group3_gbtModel')
+
+
+
+#normalized_df.write.mode('overwrite').parquet('/rawzone/')
 
--- I.py
+++ I.py
@@ -9,12 +9,12 @@ from airflow.providers.google.cloud.operators.dataproc import (
 from airflow.providers.google.cloud.sensors.dataproc import DataprocJobSensor
 from airflow.utils.dates import days_ago
 
-PROJECT_ID = "playground-s-11-82e027bf"
-CLUSTER_NAME =  "pim30-tanat-classification-model"
+PROJECT_ID = "playground-s-11-9f300151"
+CLUSTER_NAME =  "group3-pim30-rf-gbt"
 REGION = "australia-southeast1"
 ZONE = "australia-southeast1-a"
-PYSPARK_URI_csv_to_HDFS = "gs://20221027-pim30-tanat-tonguthaisri-au-southeast1/refinedzone/G.py"
-PYSPARK_URI_HDFS_to_Model = "gs://20221027-pim30-tanat-tonguthaisri-au-southeast1/refinedzone/H.py"
+PYSPARK_URI_csv_to_Model = "gs://20221027-pim30-tanat-tonguthaisri-au-southeast1/refinedzone/G.py"
+
 
 YESTERDAY = datetime.datetime.now() - datetime.timedelta(days=1)
 
@@ -39,12 +39,12 @@ CLUSTER_CONFIG = ClusterGenerator(
     worker_machine_type="n1-standard-2",
     worker_disk_type="pd-standard",
     worker_disk_size=512,
-    properties={"spark:spark.jars.packages":"com.microsoft.azure:spark-mssql-connector:1.0.2","dataproc:dataproc.conscrypt.provider.enable":"false"}
+    properties={"dataproc:dataproc.conscrypt.provider.enable":"false"}    
 ).make()
 
 
 with models.DAG(
-   "Spark-HDFS-SparkML-GCS",
+   "Spark-HDFS-SparkML-GCS-02",
    catchup=False,
    schedule_interval=datetime.timedelta(minutes=30),
    default_args=default_dag_args) as dag:
@@ -65,25 +65,14 @@ with models.DAG(
        cluster_name=CLUSTER_NAME,
    )
 
-   PYSPARK_JOB_csv_to_HDFS = {
+   PYSPARK_JOB_csv_to_Model = {
    "reference": {"project_id": PROJECT_ID},
    "placement": {"cluster_name": CLUSTER_NAME},
-   "pyspark_job": {"main_python_file_uri": PYSPARK_URI_csv_to_HDFS},
-   }
-   
-   
-   PYSPARK_JOB_HDFS_to_Model = {
-   "reference": {"project_id": PROJECT_ID},
-   "placement": {"cluster_name": CLUSTER_NAME},
-   "pyspark_job": {"main_python_file_uri": PYSPARK_URI_HDFS_to_Model},
+   "pyspark_job": {"main_python_file_uri": PYSPARK_URI_csv_to_Model},
    }
 
-   pyspark_task_csv_to_HDFS = DataprocSubmitJobOperator(
-       task_id="pyspark_task_csv_to_HDFS", job=PYSPARK_JOB_csv_to_HDFS, region=REGION, project_id=PROJECT_ID
-   )
-   
-   pyspark_task_HDFS_to_Model = DataprocSubmitJobOperator(
-       task_id="pyspark_task_HDFS_to_Model", job=PYSPARK_JOB_HDFS_to_Model, region=REGION, project_id=PROJECT_ID
+   pyspark_task_csv_to_Model = DataprocSubmitJobOperator(
+       task_id="pyspark_task_csv_to_Model", job=PYSPARK_JOB_csv_to_Model, region=REGION, project_id=PROJECT_ID
    )
 
-   create_cluster >>  pyspark_task_csv_to_HDFS >> pyspark_task_HDFS_to_Model >> delete_cluster
+   create_cluster >>  pyspark_task_csv_to_Model >> delete_cluster
